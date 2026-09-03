6734fcdb7a76a9bda267c844cb8243c4681eed2f
tree <sejin20100323@dimigo.hs.kr>
2026-09-03 15:30:06 +0900

    add ex1.py ~ ex3.py

---
+++ 03_control/ex2.py
@@ -0,0 +1,50 @@
+# 반복문 : while문, for문
+
+# while문
+# 1~10까지 반복 출력
+i = 1
+while i < 11:
+    print(i)
+    i += 1
+    if i == 5:
+        break
+else:
+    print("End")    # 조건식을 다 돌았을 때 나옴(정상작동 했을때)
+
+nums = [1, 3, 5, 7, 9]
+target = 2
+
+i = 0
+while i < len(nums):
+    if nums[i] == target:
+        print(f"{target} found.")
+        break
+    i += 1
+else:
+    print(f"{target} not found.")
+
+# 1부터 10까지의 합
+tot = 0
+i = 1
+while i < 11:
+    tot += i
+    i += 1
+else:
+    print(f"합: {tot}")
+
+
+while i < 11:
+    if i%2 == 0:
+        tot += i
+    i += 1
+else:
+    print(f"합: {tot}")
+
+
+while i < 11:
+    i += 1
+    if i%2 != 0:
+        continue
+    tot += i
+else:
+    print(f"합: {tot}")
